ae2e154dca8fcd49d68c76dd2c1f3cc7a9f1e6d9
Katy Daviscourt and Nicole Sanders <katy+nicole@>
2017-02-07 10:09:23 -0800

    make form page and page to display the added car

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using Cars.Objects;
+using System.Collections.Generic;
+
+namespace CarDealership
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      
+    }
+  }
+}
