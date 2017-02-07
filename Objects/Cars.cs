53935dd650cd67c80930a53535ecd0e20d36f5de
Katy Daviscourt and Nicole Sanders <katy+nicole@>
2017-02-07 09:57:22 -0800

    add initial files and the car class with getters/setters, private variables, and a constructor

---
+++ Objects/Cars.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+
+namespace Cars.Objects
+{
+  public class Car
+  {
+    private string _makeModel;
+    private int _miles;
+    private int _price;
+
+    public string GetMakeModel()
+    {
+      return _makeModel;
+    }
+
+    public void SetMakeModel(string newMakeModel)
+    {
+      _makeModel = newMakeModel;
+    }
+
+    public int GetMiles()
+    {
+      return _miles;
+    }
+
+    public void SetMiles(int newMiles)
+    {
+      _miles = newMiles;
+    }
+
+    public int GetPrice()
+    {
+      return _price;
+    }
+
+    public void SetPrice(int newPrice)
+    {
+      _price = newPrice;
+    }
+
+    public Car(string carMakeModel, int carMiles, int carPrice)
+    {
+      _makeModel = carMakeModel;
+      _miles = carMiles;
+      _price = carPrice;
+    }
+
+
+  }
+
+}
